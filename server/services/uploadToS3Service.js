e19c84bc74c42f4b2a14b7f14b8e535fac952495
Namrata Karmakar <namrata.krmkr@gmail.com>
2023-06-11 22:29:02 +0200

    added async, try-catch

--- server/services/uploadToS3Service.js
+++ server/services/uploadToS3Service.js
@@ -3,17 +3,17 @@ import fs from 'fs';
 
 class UploadToS3Service {
 
-    uploadCsvToS3(fileName) {
+    async uploadCsvToS3(fileName) {
+        let response, status;
+
         AWS.config.update({
             accessKeyId: process.env.ACCESS_KEY_ID,
             secretAccessKey: process.env.SECRET_ACCESS_KEY,
             region: 'eu-central-1'
         });
 
-        const s3 = new AWS.S3();
-
         // Read the CSV file
-        const fileContent = fs.readFileSync(`datasets/${fileName}.csv`);
+        const fileContent = fs.readFileSync(`datasets/${fileName}.csv`, { encoding: "utf8" });
 
         // Set the parameters for S3 bucket and file upload
         const params = {
@@ -22,14 +22,22 @@ class UploadToS3Service {
             Body: fileContent
         };
 
-        // Upload the file to S3
-        s3.upload(params, (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(`File ${fileName} uploaded successfully.`, data.Location);
-            }
-        });
+        try {
+            const s3 = new AWS.S3();
+
+            // Upload the file to S3
+            s3.upload(params, (err, data) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(`File ${fileName} uploaded successfully.`, data.Location);
+                }
+            });
+        } catch (error) {
+            response = error.message;
+            status = 500;
+            throw error;
+        }
     }
 
 }
